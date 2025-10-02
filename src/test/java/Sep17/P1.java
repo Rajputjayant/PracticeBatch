296a6682f152963ffeaaf4ec05da0034412d44fa
Rajputjayant <Singh.jayant.jayant84@gmail.com>
2025-10-02 20:38:50 +0530

    new added scrip

--- src/test/java/Sep17/P1.java
+++ src/test/java/Sep17/P1.java
@@ -2,8 +2,11 @@ package Sep17;
 
 import org.testng.Reporter;
 
+import java.sql.SQLOutput;
+
 public class P1 {
     static int a =50;
+    int b =100;
     public static void main(String[] args) {
 
         System.out.println("Jayant Rajput");
@@ -17,6 +20,8 @@ public class P1 {
         System.out.println("Jayant");
         System.out.println("Jayant");
         System.out.println("Jayant");
+         P1 p1 = new P1();
+        System.out.println(p1.b);
 
     }
 }
