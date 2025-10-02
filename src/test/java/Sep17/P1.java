5e3fb33d1a8bef51cdea3ae380fadf2ad951d354
Rajputjayant <110388079+Rajputjayant@users.noreply.github.com>
2025-10-02 15:10:16 +0530

    second commit  (#1)
    * second
    
    * review comment

---
+++ src/test/java/Sep17/P1.java
@@ -0,0 +1,12 @@
+package Sep17;
+
+import org.testng.Reporter;
+
+public class P1 {
+    static int a =50;
+    public static void main(String[] args) {
+
+        System.out.println("Jayant Rajput");
+        Reporter.log("Print"+ P1.a,true);
+    }
+}
