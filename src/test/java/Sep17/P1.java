23a3572ef068ec130ee7c6b3a702631020592df9
Rajputjayant <Singh.jayant.jayant84@gmail.com>
2025-10-02 15:07:33 +0530

    review comment

--- src/test/java/Sep17/P1.java
+++ src/test/java/Sep17/P1.java
@@ -1,10 +1,12 @@
 package Sep17;
 
+import org.testng.Reporter;
+
 public class P1 {
     static int a =50;
     public static void main(String[] args) {
 
         System.out.println("Jayant Rajput");
-        System.out.println(P1.a);
+        Reporter.log("Print"+ P1.a,true);
     }
 }
