122d22cfbe5b24445db31fadaca98a9ae4dff960
Rajputjayant <Singh.jayant.jayant84@gmail.com>
2025-10-02 15:01:56 +0530

    second

---
+++ src/test/java/Sep17/P1.java
@@ -0,0 +1,10 @@
+package Sep17;
+
+public class P1 {
+    static int a =50;
+    public static void main(String[] args) {
+
+        System.out.println("Jayant Rajput");
+        System.out.println(P1.a);
+    }
+}
