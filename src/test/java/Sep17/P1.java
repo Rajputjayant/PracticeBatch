9f0feb7d5c8b3d83125c58b082aea54080c91cfd
Rajputjayant <110388079+Rajputjayant@users.noreply.github.com>
2025-10-02 22:25:09 +0530

    Qable (#3)
    * added new script
    
    * new changes
    
    * pom.xml
    
    * message
    
    * new added scrip
    
    * add new

--- src/test/java/Sep17/P1.java
+++ src/test/java/Sep17/P1.java
@@ -2,13 +2,27 @@ package Sep17;
 
 import org.testng.Reporter;
 
+import java.sql.SQLOutput;
+
 public class P1 {
     static int a =50;
+    int b =100;
     public static void main(String[] args) {
 
         System.out.println("Jayant Rajput");
         Reporter.log("Print"+ P1.a,true);
         Reporter.log("Print"+ P1.a,true);
+        System.out.println("Sumit Gaya Ji ");
+        System.out.println("Sumit Gaya Ji ");
+        System.out.println("Sumit Gaya Ji ");
         System.out.println("Sumit Gaya Ji ")
+        System.out.println("Jayant");
+        System.out.println("Jayant");
+        System.out.println("Jayant");
+        System.out.println("Jayant");
+        System.out.println("Jayant");
+         P1 p1 = new P1();
+        System.out.println(p1.b);
+        System.out.println(p1.b);
     }
 }
